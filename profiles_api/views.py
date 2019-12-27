803ef09bd848a7ac42f9b2ec214f5bd49b0c6776
Manthankumar Satani <mnthnup@gmail.com>
2019-12-27 18:53:27 +0530

    Added Profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,13 +1,15 @@
-from rest_framework.views import APIView
+from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import viewsets
-from profiles_api import  serializers
+from rest_framework.views import APIView
+
+from profiles_api import models, serializers, permissions
+
 
 class HelloApiView(APIView):
     """Test API view"""
     serializer_class = serializers.HelloSerializer
-    
+
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
@@ -17,7 +19,7 @@ class HelloApiView(APIView):
             "Is mapped manually to URLs",
         ]
 
-        return Response({'message':"Hello!", "an_apiview": an_apiview})
+        return Response({'message': "Hello!", "an_apiview": an_apiview})
 
     def post(self, request):
         """Create a hello message with our name"""
@@ -29,34 +31,35 @@ class HelloApiView(APIView):
             return Response({"message": message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     def put(self, request, pk=None):
         """Handle updating an object"""
-        return Response({"method":"PUT"})
-    
+        return Response({"method": "PUT"})
+
     def patch(self, request, pk=None):
         """Handle partial updating an object"""
-        return Response({"method":"PATCH"})
-    
+        return Response({"method": "PATCH"})
+
     def delete(self, request, pk=None):
         """Delete an object"""
-        return Response({"method":"DELETE"})
+        return Response({"method": "DELETE"})
+
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API View Set"""
-    
+
     serializer_class = serializers.HelloSerializer
-    
+
     def list(self, request):
         """Return a hello message"""
-        
+
         a_viewset = [
             "Uses actions (list, create retrive update, partial_update)",
             "Automatically match to URLs using Routers",
             "Provides more functionality with less code",
         ]
 
-        return Response({"message":"Hello!", "a_viewset":a_viewset})
+        return Response({"message": "Hello!", "a_viewset": a_viewset})
 
     def create(self, request):
         """Create a new hello message"""
@@ -64,25 +67,34 @@ class HelloViewSet(viewsets.ViewSet):
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f"Hello {name}"
-            return Response({"message":message})
+            return Response({"message": message})
         else:
             return Response(
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
-    
+
     def retrieve(self, request, pk=None):
         """Handle getting an object by it's ID"""
-        return Response({"http_method":"GET"})
-    
+        return Response({"http_method": "GET"})
+
     def update(self, request, pk=None):
         """Handle updating an object"""
-        return Response({"http_method":"PUT"})
-    
+        return Response({"http_method": "PUT"})
+
     def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
-        return Response({"http_method":"PATCH"})
-    
+        return Response({"http_method": "PATCH"})
+
     def destroy(self, request, pk=None):
         """Handle removing objects"""
-        return Response({"http_method":"DELETE"})
+        return Response({"http_method": "DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profile"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile, )
